c7238094f316edf817f4e1c5ba37c02ae9cb76c5
dvillagracia <daniellejvillagracia@gmail.com>
2020-02-08 18:17:08 +0800

    changes to welcome page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,6 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src=""></script>
     <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.js"></script>
--- resources/views/nurses/createcharts.blade.php
+++ resources/views/nurses/createcharts.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-	<h1>Create charts for patient. lol</h1>
+	<h1>Patient Chart</h1>
 
 	<div>
 		<div class="col-md-6">
             <select name="role" class="form-control" >
-            <option value="">Doctor</option>
-            <option value="admission">Admission</option>
-            <option value="nurse">Nurse</option>
-            <option value="headNurse">Head Nurse</option>
-            <option value="medRecords">Medical Records</option>
+            <option value="vitalsign">Vital Sign Monitoring</option>
+            <option value="rbs">RBS Monitoring</option>
+            <option value="dengue">Dengue Monitoring </option>
+            <option value="medication">Medication Record</option>
+            <option value="intakeoutput">Intake and Output Record</option>
+            <option value="intravenous">Intravenous Fluids Record</option>
             </select> 
         </div>
 	</div>
