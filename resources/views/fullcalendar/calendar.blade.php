0bc6e46bd08777d20eb80c01f2ac820f709d0ee7
livingtwentytwo <nikkithea15@gmail.com>
2020-02-22 13:52:48 +0800

    ko

--- resources/views/fullcalendar/calendar.blade.php
+++ resources/views/fullcalendar/calendar.blade.php
@@ -4,10 +4,9 @@
     <meta charset='utf-8' />
 
     
-    <script src="~/scripts/fullcalendar/core/main.js"></script>
-    <script src="~/scripts/fullcalendar/daygrid/main.js"></script>
-    <script src="~/scripts/jquery-3.4.1.js"></script>
-    <script scr ="~/scripts/moment.js"></script>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script scr ="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
     <script scr ="js/calendar.js"></script>
 
     <script scr ="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
